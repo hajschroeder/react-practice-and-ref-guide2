52aed5252e88a908ca458b66947b0e93572acc28
Hans Schroeder <hajschroeder@gmail.com>
2023-04-18 19:49:08 -0700

    include card and button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const addUserHandler = (event) =>{
+  const [enteredUsernameState, setEnteredUsernameState] = useState('');
+  const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
+
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input id='username' type='text' />
-      <label htmlFor='age'>Age</label>
-      <input id='age' type='number'></input>
-      <button type='submit'>Submit</button>
-    </form>
-  )
-}
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUser;
