85810cbfc1a92fefc5791d35450ea79ff9482565
Hans Schroeder <hajschroeder@gmail.com>
2023-04-28 12:51:01 -0700

    include UsersList component. Import into App

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,8 +1,24 @@
-import './UsersList.module.css';
+import classes from './UsersList.module.css';
 import React from 'react';
+import Card from '../UI/Card';
 
-const UsersList = () => {
+const UsersList = (props) => {
+  return (
+    <Card classname={classes.users}>
+      <ul>
+        {/* We're using props to pass the information between components */}
+        {/* The props is what is giving us our array of users as data */}
+        {/* map() is transforming our list of users into JSX elements to appear in the DOM. It executes a function as every item in the array */}
+        {/* usersList is now returning an array where every element has a name and an age property */}
 
+        {props.usersList.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  )
 }
 
 export default UsersList;
